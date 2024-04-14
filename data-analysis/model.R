3da7a3162c7af94b89f70e82203d0312f847e5d2
kopy-kat <kopp.konrad@gmail.com>
2024-04-14 15:17:30 +0200

    method and data analysis sections

--- data-analysis/model.R
+++ data-analysis/model.R
@@ -4,68 +4,50 @@ library(lsr)
 
 # Load the experiment data
 experiment_data <-
-  read.csv("virtual-currencies-paper/data/experiment_data.csv")
-
-# Rename the columns to more meaningful names
-colnames(experiment_data)[which(names(experiment_data) == "Q5a")] <-
-  "group1_bid"
-colnames(experiment_data)[which(names(experiment_data) == "Q5b")] <-
-  "group2_bid"
-colnames(experiment_data)[which(names(experiment_data) == "Q6")] <-
-  "lived_abroad"
-colnames(experiment_data)[which(names(experiment_data) == "Q7")] <-
-  "travel_frequency"
-colnames(experiment_data)[which(names(experiment_data) == "Q8")] <-
-  "expenditure"
-
-# Convert the values of the bid columns to pounds
-experiment_data$group1_bid_in_pounds <-
-  experiment_data$group1_bid * 5
-experiment_data$group2_bid_in_pounds <-
-  experiment_data$group2_bid / 5
+  read.csv("virtual-currencies-paper/data/experiment_data_formatted.csv")
 
 # Summary statistics for the bid columns
-summary(experiment_data$group1_bid_in_pounds)
-sd(experiment_data$group1_bid_in_pounds, na.rm = TRUE)
+summary(experiment_data$group_fraction_bid_in_pounds)
+sd(experiment_data$group_fraction_bid_in_pounds, na.rm = TRUE)
 
-summary(experiment_data$group2_bid_in_pounds)
-sd(experiment_data$group2_bid_in_pounds, na.rm = TRUE)
+summary(experiment_data$group_multiple_bid_in_pounds)
+sd(experiment_data$group_multiple_bid_in_pounds, na.rm = TRUE)
 
 # Plot a boxplot for the bid columns
 boxplot(
-  experiment_data$group1_bid_in_pounds,
-  experiment_data$group2_bid_in_pounds,
-  main = "Boxplot of group 1 and group 2 bids (in Pounds)",
-  names = c("Group 1", "Group 2"),
+  experiment_data$group_fraction_bid_in_pounds,
+  experiment_data$group_multiple_bid_in_pounds,
+  main = "Boxplot of fraction and multiple group bids (in Pounds)",
+  names = c("Group Fraction", "Group Multiple"),
   xlab = "Variables",
   ylab = "Values"
 )
 
 # Perform a t-test to compare the means of the two groups
 t.test(
-  experiment_data$group1_bid_in_pounds,
-  experiment_data$group2_bid_in_pounds
+  experiment_data$group_fraction_bid_in_pounds,
+  experiment_data$group_multiple_bid_in_pounds
 )
 
 # Conduct a regression analysis to examine the relationship between
 # the bid amounts and the demographic variables
-model_group1 <-
-  lm(group1_bid_in_pounds ~ lived_abroad + travel_frequency + expenditure,
+model_group_fraction <-
+  lm(group_fraction_bid_in_pounds ~ lived_abroad + travel_frequency + expenditure,
     data = experiment_data
   )
-summary(model_group1)
+summary(model_group_fraction)
 
-model_group2 <-
-  lm(group2_bid_in_pounds ~ lived_abroad + travel_frequency + expenditure,
+model_group_multiple <-
+  lm(group_multiple_bid_in_pounds ~ lived_abroad + travel_frequency + expenditure,
     data = experiment_data
   )
-summary(model_group2)
+summary(model_group_multiple)
 
 # Conduct a power analysis to determine the power of the t test and
 # sample size required for a power of 0.8 (80%)
 effect_size <- cohensD(
-  experiment_data$group1_bid_in_pounds,
-  experiment_data$group2_bid_in_pounds
+  experiment_data$group_fraction_bid_in_pounds,
+  experiment_data$group_multiple_bid_in_pounds
 )
 pwr.t2n.test(n1 = 21, n2 = 20, d = effect_size, sig.level = 0.05)
 pwr.t.test(power = 0.8, d = effect_size, sig.level = 0.05)
